330e00e3a852ee711e997ac8569c85e75341f49b
Manikandankasirajan <itsmanikandankasirajan@gmail.com>
2025-10-25 15:22:13 +0530

    chore : added a new funtion createOptimisedBookSets in customCheckOutRule to handle all the logic behind creating optimised booksets for better discount

--- src/rules/customCheckOutRule.js
+++ src/rules/customCheckOutRule.js
@@ -2,40 +2,16 @@ import calculatePriceAfterDiscount from "../utils/calculatePriceAfterDiscount";
 import extractBookTitlesFromCart from "../utils/extractBookTitlesFromCart";
 import createBookMapWithTitleAndCount from "../utils/createBookMapWithTitleAndCount";
 import createBookSetsFromBookMap from "../utils/createBookSetsFromBookMap";
+import createOptimisedBookSets from "../utils/createOptimisedBookSets";
 
 export default function customCheckOutRule(cart) {
 	const bookTitlesFromCart = extractBookTitlesFromCart(cart);
 	const bookMap = createBookMapWithTitleAndCount(bookTitlesFromCart);
 	let bookSets = createBookSetsFromBookMap(bookMap);
-	const bookSetAndCountMap = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
-	for (const bookSet of bookSets) {
-		if (bookSet.length === 3 || bookSet.length === 5) {
-			bookSetAndCountMap[bookSet.length]++;
-		}
-	}
-	if (bookSetAndCountMap[3] > 0 && bookSetAndCountMap[5] > 0) {
-		const optimisationPairs = Math.min(
-			bookSetAndCountMap[3],
-			bookSetAndCountMap[5]
-		);
-
-		for (let i = 0; i < optimisationPairs; i++) {
-			bookSetAndCountMap[3]--;
-			bookSetAndCountMap[5]--;
-			bookSets.push(Array(4).fill("Placeholder"));
-			bookSets.push(Array(4).fill("Placeholder"));
-		}
-
-		const optimisedBookSets = bookSets.filter(
-			(set) => set.length !== 5 && set.length !== 3
-		);
-
-		bookSets = optimisedBookSets;
-	}
-
+	const optimisedBookSets = createOptimisedBookSets(bookSets);
 	let total = 0;
-	for (const bookSet of bookSets) {
-		total += calculatePriceAfterDiscount(bookSet);
+	for (const optimisedBookSet of optimisedBookSets) {
+		total += calculatePriceAfterDiscount(optimisedBookSet);
 	}
 	return total;
 }
---
+++ src/utils/createOptimisedBookSets.js
@@ -0,0 +1,29 @@
+export default function createOptimisedBookSets(bookSets) {
+	const bookSetAndCountMap = { 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
+	for (const bookSet of bookSets) {
+		if (bookSet.length === 3 || bookSet.length === 5) {
+			bookSetAndCountMap[bookSet.length]++;
+		}
+	}
+	if (bookSetAndCountMap[3] > 0 && bookSetAndCountMap[5] > 0) {
+		const optimisationPairs = Math.min(
+			bookSetAndCountMap[3],
+			bookSetAndCountMap[5]
+		);
+
+		for (let i = 0; i < optimisationPairs; i++) {
+			bookSetAndCountMap[3]--;
+			bookSetAndCountMap[5]--;
+			bookSets.push(Array(4).fill("Placeholder"));
+			bookSets.push(Array(4).fill("Placeholder"));
+		}
+
+		const optimisedBookSets = bookSets.filter(
+			(set) => set.length !== 5 && set.length !== 3
+		);
+
+		return optimisedBookSets;
+	} else {
+		return bookSets;
+	}
+}
