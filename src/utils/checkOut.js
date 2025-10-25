f3661492dd0c5cbfaa3d8f6c59bb6df16eec29f7
Manikandankasirajan <itsmanikandankasirajan@gmail.com>
2025-10-25 13:33:47 +0530

    chore : modified the checkout() function by replacing the ifelse condition to ternary operator for simplification

--- src/utils/checkOut.js
+++ src/utils/checkOut.js
@@ -3,9 +3,7 @@ import defaultCheckOutRule from "../rules/defaultCheckOutRule";
 import customCheckOutRule from "../rules/customCheckOutRule";
 
 export default function checkOut(cart) {
-	if (hasDuplicateItemsInCart(cart)) {
-		return customCheckOutRule(cart);
-	} else {
-		return defaultCheckOutRule(cart);
-	}
+	return hasDuplicateItemsInCart(cart)
+		? customCheckOutRule(cart)
+		: defaultCheckOutRule(cart);
 }
